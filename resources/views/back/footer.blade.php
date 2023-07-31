1f95a77df8d770cfaff3611d60233dd480c0b199
Arya Bayu <aryabayu23@gmail.com>
2023-07-31 11:35:33 +0700

    footer link back

--- resources/views/back/footer.blade.php
+++ resources/views/back/footer.blade.php
@@ -6,7 +6,7 @@
           Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
         </div> --}}
         <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
-          <a class="fw-semibold" href="" target="_blank">SUBMARINE RECORDS</a> &copy; <span data-toggle="year-copy"></span>
+          <a class="fw-semibold" href="" target="_blank">Submarine Records</a> &copy; <span data-toggle="year-copy"></span>
         </div>
       </div>
     </div>
